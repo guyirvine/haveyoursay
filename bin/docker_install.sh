a1148175469d77f42c89dca718692bcf727b5174
Guy Irvine <guyirvine@gmail.com>
2016-09-09 06:21:11 +1200

    Fix #32

--- bin/docker_install.sh
+++ bin/docker_install.sh
@@ -1,10 +1,10 @@
-apt-get install -y apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get -y update
-apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine
-usermod -aG docker vagrant
+# apt-get install -y apt-transport-https ca-certificates
+# apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+# apt-get -y update
+# apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine
+# usermod -aG docker vagrant
 
-# docker build -t haveyoursay .
-# docker stop $(docker ps -q --filter ancestor=haveyoursay)
-# source ~/.env && docker run -d -e "DB=$HAVEYOURSAY_DB" --net=host haveyoursay
+docker stop $(docker ps -q --filter ancestor=haveyoursay)
+docker build -t haveyoursay .
+source ~/.env && docker run -d -e "DB=$HAVEYOURSAY_DB" --net=host haveyoursay
