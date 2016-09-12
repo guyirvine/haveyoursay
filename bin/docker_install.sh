30f6e418a62d2dca148520983c9e8b0800ffc9ea
Guy Irvine <guyirvine@gmail.com>
2016-09-13 08:57:28 +1200

    Fix #41

--- bin/docker_install.sh
+++ bin/docker_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # apt-get install -y apt-transport-https ca-certificates
 # apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 # echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
@@ -6,5 +7,5 @@
 # usermod -aG docker vagrant
 
 docker stop $(docker ps -q --filter ancestor=haveyoursay)
-docker build -t haveyoursay .
+docker build -t haveyoursay "$(dirname "$0")/../"
 source ~/.env && docker run -d -e "DB=$HAVEYOURSAY_DB" --net=host haveyoursay
