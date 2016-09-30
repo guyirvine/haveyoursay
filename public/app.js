a619ee602a210e6bb36160e800ebb6b82dc818c4
Guy Irvine <guyirvine@gmail.com>
2016-09-30 16:24:22 +1300

    Fix #71

--- public/app.js
+++ public/app.js
@@ -125,17 +125,17 @@ app.initialise_card = function (card) {
         if (card.newness() === 'newest-card' ) {
             var hours = moment().diff(moment(card.createdon), 'hours');
             if (hours === 1) {
-                return '1 hour ago.';
+                return '1 hour ago. ';
             }
-            return hours + ' hours ago.';
+            return hours + ' hours ago. ';
         }
 
         if (card.newness() === 'new-card' || card.newness() === 'newish-card') {
             var days = moment().diff(moment(card.createdon), 'days');
             if (days === 1) {
-                return  + '1 day ago';
+                return  + '1 day ago ';
             }
-            return days + ' days ago';
+            return days + ' days ago ';
         }
 
         return '';
